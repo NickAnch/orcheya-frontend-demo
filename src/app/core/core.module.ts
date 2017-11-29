4086c3bc2a9118974c656fbdeb4e4c6badde6f15
ikasparov <ikasparov.dev@gmail.com>
2017-11-29 15:51:09 +0300

    Add basic login page to core. #TT-12

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { LoginPage } from './pages/login/login.page';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: []
+  declarations: [LoginPage]
 })
 export class CoreModule { }
