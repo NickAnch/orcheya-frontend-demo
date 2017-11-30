7582587be0ed8d389c0cc9c56109526fc3f8865e
ikasparov <ikasparov.dev@gmail.com>
2017-11-30 12:17:44 +0300

    Start commecting admin lte to header and wrappers. #TT-12

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { WrapperPage } from './pages/wrapper/wrapper.page';
 import { LoginPage } from './pages/login/login.page';
 
 const routes: Routes = [
   {
     path: '',
+    component: WrapperPage,
     children: []
   },
   {
