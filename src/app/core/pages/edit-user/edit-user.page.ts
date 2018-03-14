43ac18c622653ece867c0a6abe924d05573ca153
iamsouthernbear <lasouthernbear@gmail.com>
2018-03-14 19:32:52 +0300

    Add time select and input for avatar upload #18

--- src/app/core/pages/edit-user/edit-user.page.ts
+++ src/app/core/pages/edit-user/edit-user.page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.page.html',
   styleUrls: ['./edit-user.page.scss']
 })
-export class EditUserPage { }
+export class EditUserPage {
+  @ViewChild('upload') public upload;
+
+  constructor() { }
+
+  public inputClickEmitter(): void {
+    this.upload.nativeElement.click();
+  }
+
+}
