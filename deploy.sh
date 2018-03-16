f85459011ac1f6b83d3e287d415bebe5b592581c
ikasparov <ikasparov.dev@gmail.com>
2018-03-16 15:07:49 +0300

    fix(deploy): Modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,15 @@
 export SERVER_IP="46.254.19.100"
 export SERVER_PORT="42777"
 export USERNAME="deploy"
+export BRANCH_NAME="master"
 
 ssh -A $USERNAME@$SERVER_IP -p $SERVER_PORT '
   cd /srv/orcheya-client;
   export NVM_DIR="/home/deploy/.nvm";
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh";
   nvm use 9.3.0;
+  git fetch;
+  git checkout $BRANCH_NAME;
   git pull origin master --force;
   yarn install;
   yarn build
