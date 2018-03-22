9ac6a2c8aa058e43725708d275df613e4d4832d0
d4dd4r <prokhor2253@gmail.com>
2018-03-22 10:49:55 +0300

    added link to own profile settings #39

--- src/app/core/pages/user-profile/user-profile.page.ts
+++ src/app/core/pages/user-profile/user-profile.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, ViewChild,
+  AfterViewChecked, AfterViewInit, ChangeDetectorRef
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap';
 
 import { CurrentUserService } from '../../services/current-user.service';
 import { User } from '../../models/user';
@@ -8,12 +13,16 @@ import { User } from '../../models/user';
   templateUrl: './user-profile.page.html',
   styleUrls: ['./user-profile.page.scss']
 })
-export class UserProfilePage implements OnInit {
+export class UserProfilePage implements OnInit, AfterViewInit, AfterViewChecked {
 
-  public currentSheet = 'activity';
+  @ViewChild('tabset') public tabset: TabsetComponent;
   public currentUser: User;
 
-  constructor(private _currentUserService: CurrentUserService) {}
+  constructor(
+    private _route: ActivatedRoute,
+    private _cdr: ChangeDetectorRef,
+    private _currentUserService: CurrentUserService
+  ) {}
 
   ngOnInit() {
     this._currentUserService
@@ -22,12 +31,31 @@ export class UserProfilePage implements OnInit {
     ;
   }
 
-  public selectSheet(sheet: string) {
-    this.currentSheet = sheet;
+  ngAfterViewInit() {
+    this._checkActiveTab();
+  }
+
+  ngAfterViewChecked() {
+    this._cdr.detectChanges();
   }
 
   public onUserUpdate(user: User) {
     this.currentUser = user;
+  }
+
+  private _checkActiveTab() {
+    if (!this._route.snapshot.queryParamMap.has('tab')) {
+      return;
+    }
+
+    const activeTabName = this._route.snapshot.queryParamMap.get('tab');
+    const currentTab = this.tabset.tabs
+      .find((tab: TabDirective) => tab.id === activeTabName)
+    ;
+
+    if (currentTab) {
+      currentTab.active = true;
+    }
   }
 }
 
