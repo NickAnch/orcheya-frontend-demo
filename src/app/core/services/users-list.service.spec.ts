bc7d72c36d27cef8758f35b5044e1ed92db5c39a
ikasparov <ikasparov.dev@gmail.com>
2018-03-26 14:33:44 +0300

    fix(specs): remove component's specs. Fix circle config

--- src/app/core/services/users-list.service.spec.ts
+++ src/app/core/services/users-list.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { UsersListService } from './users-list.service';
 
 describe('UsersListService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [UsersListService]
     });
   });
