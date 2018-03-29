c4c7072c291bed612a223365cce39e6873a5ffe4
d4dd4r <prokhor2253@gmail.com>
2018-03-29 18:24:26 +0300

    eslint fixes

--- src/app/core/components/time-this-week/time-this-week.component.ts
+++ src/app/core/components/time-this-week/time-this-week.component.ts
@@ -20,11 +20,34 @@ export class TimeThisWeekComponent implements OnInit {
   private totalWeekTime = 0;
 
   ngOnInit() {
-
     this.activity
       .subscribe(data => (
         this.updateTimes(data.splice(-7))
       ));
+  }
+
+  private static getMonday(): Date {
+    const date = new Date();
+    const day = date.getDay();
+
+    if (day === 0) {
+      date.setDate(date.getDate() - 6);
+    } else if (day > 1) {
+      date.setDate(date.getDate() - day + 1);
+    }
+
+    return date;
+  }
+
+  private static convertTime(time: number): string {
+    if (!time) {
+      return '0';
+    }
+
+    const hours = Math.floor(time / 60);
+    const minutes = time % 60;
+
+    return `${hours}h ${minutes}m`;
   }
 
   private updateTimes(activity: TimeActivity[]) {
@@ -40,33 +63,12 @@ export class TimeThisWeekComponent implements OnInit {
       const activityPeace = activity.find(d => d.date === strDate);
       this.totalWeekTime += activityPeace ? activityPeace.time : 0;
 
-      return activityPeace ? TimeThisWeekComponent.convertTime(activityPeace.time) : '0';
+      return activityPeace
+        ? TimeThisWeekComponent.convertTime(activityPeace.time)
+        : '0';
     });
 
-    this.totalWeekTimeText = TimeThisWeekComponent.convertTime(this.totalWeekTime);
-  }
-
-  private static getMonday(): Date {
-    const date = new Date();
-    const day = date.getDay();
-
-    if (day === 0) {
-      date.setDate(date.getDate() - 6);
-    } else if (day > 1) {
-      date.setDate(date.getDate() - day + 1);
-    }
-console.log(date);
-    return date;
-  }
-
-  private static convertTime(time: number): string {
-    if (!time) {
-      return '0';
-    }
-
-    const hours = Math.floor(time / 60);
-    const minutes = time % 60;
-
-    return `${hours}h ${minutes}m`;
+    this.totalWeekTimeText = TimeThisWeekComponent
+      .convertTime(this.totalWeekTime);
   }
 }
