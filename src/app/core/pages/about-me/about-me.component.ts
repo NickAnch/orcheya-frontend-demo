3bf81ccb6eb5afdbe5d37bb20621f2c2790be226
d4dd4r <prokhor2253@gmail.com>
2018-04-04 17:50:30 +0300

    added styles

--- src/app/core/pages/about-me/about-me.component.ts
+++ src/app/core/pages/about-me/about-me.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './about-me.component.html'
+  templateUrl: './about-me.component.html',
+  styles: [`
+    :host {
+      background: #d2d6de;
+      display: block;
+      min-height: 100%;
+    }
+  `]
 })
-export class AboutMeComponent {
-
-}
+export class AboutMeComponent {}
