70ec87ccef03aeab2d525ea76a61f0c2df1b96a0
d4dd4r <prokhor2253@gmail.com>
2018-04-06 14:43:28 +0300

    updated sidebar logic

--- src/app/shared/sidebar/sidebar.service.ts
+++ src/app/shared/sidebar/sidebar.service.ts
@@ -6,30 +6,7 @@ export class SidebarService {
   private _menu: IMenuGroup[] = [];
 
   constructor() {
-    // TODO : Test menu
     this._menu = [
-      {
-        name: 'Test 1',
-        icon: 'fa-dashboard',
-        single: false,
-        items: [
-          { name: 'test 1', link: ['test1'] },
-          { name: 'test 2', link: ['test2'] },
-          { name: 'test 3', link: ['/sign-in'] },
-          { name: 'test 4', link: ['test4'] },
-        ]
-      },
-      {
-        name: 'Test 2',
-        icon: 'fa-telegram',
-        single: false,
-        items: [
-          { name: 'test 1', link: ['test1'] },
-          { name: 'test 2', link: ['test2'] },
-          { name: 'test 3', link: ['test3'] },
-          { name: 'test 4', link: ['test4'] },
-        ]
-      },
       {
         name: 'Test 3',
         icon: 'fa-users',
@@ -43,6 +20,7 @@ export class SidebarService {
       }
     ];
   }
+
   public get menu(): IMenuGroup[] {
     return this._menu;
   }
