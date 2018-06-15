6df8be26338e2aa9cd0236930699d60d00149116
Eugene Yak <geneayak@gmail.com>
2018-06-15 18:17:38 +0800

    Remove comments

--- src/app/reports/reports-routing.module.ts
+++ src/app/reports/reports-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportsComponent } from './reports.component';
 import { ServiceLoadPage } from './pages/service-load/service-load.page';
-// import { CanActivateAdminGuard } from './services';
 
 const routes: Routes = [
   {
     path: '',
     component: ReportsComponent,
-    // canActivate: [CanActivateAdminGuard],
     children: [
       { path: 'service-load', component: ServiceLoadPage }
     ]
