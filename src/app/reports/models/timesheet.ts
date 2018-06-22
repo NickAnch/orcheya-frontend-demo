4c0933ac33da1ea5c8f03a61625dba5b79a72e6a
Eugene Yak <geneayak@gmail.com>
2018-06-22 16:46:32 +0800

    Move models to different files

--- src/app/reports/models/timesheet.ts
+++ src/app/reports/models/timesheet.ts
@@ -1,4 +1,5 @@
 import { Alias, Model } from 'tsmodels';
+import { Worklog } from './worklog';
 
 export class TimesheetRow extends Model {
   @Alias() public id: number;
@@ -7,11 +8,8 @@ export class TimesheetRow extends Model {
   @Alias() public worklogs: Worklog[];
 
   public get time(): number {
-    return this.worklogs.map(e => e.time).reduce((acc, time) => acc + time, 0);
+    return this.worklogs
+      .map(e => e.time)
+      .reduce((acc, time) => acc + time, 0);
   }
-}
-
-export class Worklog {
-  @Alias() public date: string;
-  @Alias() public time: number;
 }
