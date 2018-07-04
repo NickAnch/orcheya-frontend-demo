a5e1dbe71c91e451a12849ddadeb55e505de8475
Eugene Yak <geneayak@gmail.com>
2018-07-04 18:12:20 +0800

    Update timing model

--- src/app/core/models/timing.ts
+++ src/app/core/models/timing.ts
@@ -1,9 +1,22 @@
 import { Alias, Model } from 'tsmodels';
+import * as moment from 'moment';
 
 /**
  This class describe of user model.
  */
 export class Timing extends Model {
   @Alias() public id: number;
-  @Alias() public time: string;
+  @Alias() public start: Date;
+  @Alias() public end: Date;
+  @Alias('flexible') public isFlexible: boolean;
+
+  get time() {
+    if (this.isFlexible) {
+      return `flexible`;
+    } else {
+      const start = moment(this.start).format('HH:mm');
+      const end = moment(this.end).format('HH:mm');
+      return `${start} - ${end}`;
+    }
+  }
 }
