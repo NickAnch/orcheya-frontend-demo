eee0f5eabd548526701fe8c95fb706c41c150111
Mark <iglad96@gmail.com>
2018-07-31 11:34:06 +0300

    Added removing user links function, added subscription on user links form, fixed layout

--- src/app/core/components/user-links/user-links.component.ts
+++ src/app/core/components/user-links/user-links.component.ts
@@ -1,38 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user-links',
   templateUrl: './user-links.component.html',
   styleUrls: ['./user-links.component.scss']
 })
-export class UserLinksComponent implements OnInit {
+export class UserLinksComponent implements OnInit, OnDestroy {
   public form: FormGroup;
   public userLinks: FormArray;
+  private _subscription: Subscription;
 
   constructor(
-    private _fb: FormBuilder
-  ) { }
+    private _fb: FormBuilder,
+  ) {}
 
   ngOnInit() {
     this.form = this._fb.group({
       userLinks: this._fb.array([this.createLink()]),
-    })
+    });
+    this.subscribeChanges();
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
   }
 
   public createLink(): FormGroup {
     return this._fb.group({
       link: '',
-    })
+    });
   }
 
   public addLink(): void {
     this.userLinks = this.form.get('userLinks') as FormArray;
     this.userLinks.push(this.createLink());
-    console.log(this.form.value);
   }
 
-  public removeUserLink(i: number): void {
-    console.log(i);
+  public removeUserLink(index: number): void {
+    const control = <FormArray>this.form.controls['userLinks'];
+    if (index !== 0) {
+      control.removeAt(index);
+    }
+  }
+
+  private subscribeChanges(): void {
+    this._subscription = this.form
+      .valueChanges
+      .debounceTime(500)
+      .subscribe((changedForm) => {
+        if (this.form.valid) {
+          console.log(changedForm);
+        }
+      });
   }
 }
