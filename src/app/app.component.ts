56a56780232acedd01c55b3e28da3196739f1802
NickAnch <kolya_anch@mail.ru>
2018-08-14 16:42:16 +0300

    fix bug of menu, screen width < 767px

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2, OnInit } from '@angular/core';
 
 /**
  * This's main component of application.
@@ -9,5 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public body = document.querySelector('body');;
+
+  @HostListener('window:resize',['$event'])
+  onResize(event) {
+    const windowWidth = event.target.innerWidth;
+    
+    if(windowWidth < 768) {
+      this.deleteSidebarCollapse();
+    }
+  }
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.checkWindowWidth();
+  }
+
+  public checkWindowWidth(): void {
+    if(window.innerWidth < 768) {
+      this.deleteSidebarCollapse();
+    }
+  }
+
+  public deleteSidebarCollapse(): void {
+    this.renderer.removeClass(this.body, 'sidebar-collapse');
+  }
 }
