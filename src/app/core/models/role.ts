7b6d5c11239577408302c2ab06fd0bd8d26407f5
NickAnch <kolya_anch@mail.ru>
2018-08-22 18:07:21 +0300

    add intentory in roles

--- src/app/core/models/role.ts
+++ src/app/core/models/role.ts
@@ -6,5 +6,6 @@ export class Role extends Model {
   @Alias('is_admin') public isAdmin = false;
   @Alias('is_developer') public isDeveloper = false;
   @Alias('is_birthday_notify') public isBirthdayNotify = false;
+  @Alias('is_inventory_notify') public isInventoryNotify = false;
   @Alias('user_count') public userCount: number;
 }
